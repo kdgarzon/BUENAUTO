853a93f3230c91b481e1afad500a141e6576f434
kdgarzon <karendanielagarzon11@gmail.com>
2023-12-01 20:01:35 -0500

    Ajuste

--- config/Conexion.php
+++ config/Conexion.php
@@ -8,8 +8,9 @@
         $link = pg_connect("host= $host dbname=$db_name user=$user password=$pass")
         or die('ERROR al conectar a la BD: ' . pg_last_error($link));
         
-        //En este caso no se necesita una función separada para seleccionar la BD puesto que,
-        //en la función inicial ya se le está indicando al motor la BD con la cual se va a trabajar
+        /*En este caso no se necesita una función separada para seleccionar la BD 
+        puesto que, en la función inicial ya se le está indicando al motor la BD con la 
+        cual se va a trabajar*/
 
         return $link;
     }

e88e39c3c1755d7a6edf1f04ab4f0e6f2df23c4f
Diego Cuervo <cyraxgamer99@gmail.com>
2023-12-06 21:24:19 +0100

    Backup y detalles

--- Paneles/Inicio/Inicio.php
+++ Paneles/Inicio/Inicio.php
@@ -14,42 +14,46 @@
     require ('../../config/Conexion.php');
     $link = conectar();
 
-    if(isset($_POST['btnBackup'])){
-        echo 'El boton fue presionado.';    
-            // Definir las credenciales de la base de datos
-            $dbHost = "localhost";
-            $dbNombre = "concesionario";
-            $dbUsuario = "postgres";
-            $dbPassword = "123456";
-    
-            // Crear el nombre del archivo de backup
-            $fecha = new DateTime();
+    function realizarBackup() {
+        // Definir las credenciales de la base de datos
+        $dbHost = 'localhost';
+        $dbNombre = 'concesionario';
+        $dbUsuario = 'postgres';
+        $dbPassword = '123456';
 
-            //$nombreArchivo = "backup_" . $fecha->format('Y-m-d_H-i-s') . ".sql";
-    
-            $nombreArchivo = __DIR__ . "/backup_" . $fecha->format('Y-m-d_H-i-s') . ".sql";
+        // Crear el nombre del archivo de backup
+        $fecha = new DateTime();
+        $nombreArchivo = "backup_" . $fecha->format('Y-m-d_H-i-s') . ".sql";
 
-            // Comando para realizar el backup
-            $comando = "pg_dump -h $dbHost -U $dbUsuario $dbNombre > $nombreArchivo";
+        // Comando para realizar el backup
+        $comando = "pg_dump -h $dbHost -U $dbUsuario $dbNombre > $nombreArchivo";
 
-            // Ejecutar el comando
-            system($comando, $output);
+        // Ejecutar el comando
+        system($comando, $output);
+        
+        // Aquí puedes agregar código para manejar la respuesta
     }
 
-
+    if(isset($_POST['btnBackup'])){
+        realizarBackup();
+        // Código para mostrar una alerta de éxito o error después de realizar el backup
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <?php include '../../config/encabezado.php';?>
-    <title>Inicio</title>
+    <title>Realizar Backup</title>
 </head>
 <body>
     <?php include '../../config/header.php';?>  
-    <h1 class = "titulo_principal">Backup</h1>
-    <hr class="linea">
     <div class="contenedor_principal">
+
         <div class="segundo">
+            <div class="cabecera">
+                <h1 class="titulo">Backup de la Base de Datos</h1>
+            </div>
+            <hr class="linea">
             <form method="POST" class="col-4 p-3 m-auto">
                 <div class="col-md-2 d-flex justify-content-between" id="boton">
                     <input type="submit" class="btn" style="background-color:#38A843" value="REALIZAR BACKUP" id="btnBackup" name="btnBackup">
